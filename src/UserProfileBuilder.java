b349a388b10b59bc670c836b085b1137ad644145
Ershanchik <aru01kzz74@gmail.com>
2026-09-10 22:27:48 +0500

    version number 2

--- src/UserProfileBuilder.java
+++ src/UserProfileBuilder.java
@@ -1,2 +1,7 @@
-public class UserProfileBuilder {
-}
+public interface UserProfileBuilder{
+    UserProfileBuilder setUsername(String username);
+    UserProfileBuilder setDisplayName(String displayName);
+    UserProfileBuilder setEmail(String email);
+    UserProfileBuilder setBio(String bio);
+    UserProfileBuilder addInterest(String interest);
+    UserProfileBuilder setVerified(boolean verified);}
