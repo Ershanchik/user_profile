45a7c8d0e0b8b8f15f05ab9f61b2f598d0710fa2
Ershanchik <aru01kzz74@gmail.com>
2026-09-11 15:03:59 +0500

    new 2

--- src/UserProfileBuilder.java
+++ src/UserProfileBuilder.java
@@ -1,7 +1,5 @@
-public interface UserProfileBuilder{
+public interface UserProfileBuilder {
     UserProfileBuilder setUsername(String username);
-    UserProfileBuilder setDisplayName(String displayName);
     UserProfileBuilder setEmail(String email);
     UserProfileBuilder setBio(String bio);
-    UserProfileBuilder addInterest(String interest);
     UserProfileBuilder setVerified(boolean verified);}
